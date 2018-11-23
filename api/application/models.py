6c0cb8d8b3d24f3f0bbaa56a3e10630fbf9db3fd
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2018-11-23 00:17:39 -0200

    Adicionando model para ler/escrever no banco

--- api/application/models.py
+++ api/application/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MalhaLogistica(models.Model):
+	nome = models.CharField(max_length=50)
+	ponto_inicial = models.CharField(max_length=1) 
+	pronto_final = models.CharField(max_length=1)
+	distancia = models.IntegerField()
+
+	def __str__(self):
+		return self.nome
